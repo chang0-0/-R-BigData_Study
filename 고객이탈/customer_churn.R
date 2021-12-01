3250bb956c633f09150e998ca681ccf8853f0274
chang0-0 <bigyoung8375@naver.com>
2021-12-01 17:10:13 +0900

    complete

---
+++ 고객이탈/customer_churn.R
@@ -0,0 +1,12 @@
+library(dplyr)
+
+setwd("C://Users//Samsung//Desktop//빅분기 실기 준비//고객이탈")
+
+ds <- read.csv("WA_Fn-UseC_-Telco-Customer-Churn.csv", header = T, fileEncoding = "UTF-8-BOM")
+summary(ds)
+
+# 1. 결측치 제거
+ds <- na.omit(ds)
+
+library(caret)
+idx <- createDataPartition(ds$Churn, p = 0.8)
